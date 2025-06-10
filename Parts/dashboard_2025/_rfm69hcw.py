6d0f9614a086439cbde75371997efb4c4f608abb
shikoubox <shikoubox@github.com>
2025-06-10 12:33:52 +0200

    More changes and tweaks to RFM code

--- Parts/dashboard_2025/_rfm69hcw.py
+++ Parts/dashboard_2025/_rfm69hcw.py
@@ -19,19 +19,21 @@ RADIO_FREQ_MHZ = 433.0  # Frequency of the radio in Mhz. Must match your
 # module! Can be a value like 915.0, 433.0, etc.
 #
 BAUD_RATE=1000
-
+BIT_RATE=1000
 # Optional encryption (MUST match on both)
 # rfm69.encryption_key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08'
 
 # Initialize RFM69 once
 try:
-    rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=BAUD_RATE, high_power=True)
+    rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, RADIO_FREQ_MHZ, bitrate=BIT_RATE, baudrate=BAUD_RATE, high_power=True)
     prev_packet = None
     print("RFM69: Detected")
-    print(f"Frequency: {rfm69.frequency_mhz}mhz")
-    print(f"Bit rate: {rfm69.bitrate}kbit/s")
+    print(f"Frequency: {rfm69.frequency_mhz}MHz")
+    print(f"Bit rate: {rfm69.bitrate}bit/s")
+    print(f"Baud rate: {rfm69.baudrate}baud/s")
     print(f"Frequency deviation: {rfm69.frequency_deviation}hz") 
     print(f"Tx_Power: {rfm69.tx_power}dBm")
+    print(f"Temperature: {rfm69.temperature}C")
 except RuntimeError as error:
     print("RFM69: ERROR")
     print("RFM69 Error:", error)
@@ -43,7 +45,6 @@ while True:
     
     if rfm69 is not None:
         # Print out some chip state:
-        print(f"Temperature: {rfm69.temperature}C")
 
         if rfm69 is not None:
             # Check for incoming packets
